4259566daa3d68ae2f2959c992f60a9f2e6f4e08
Eloping <elopingbuisness@gmail.com>
2025-05-02 15:10:09 +0200

    Update EloHub.lua

--- EloHub.lua
+++ EloHub.lua
@@ -6,6 +6,8 @@ if universeId == 7436755782 then
 elseif universeId == 5750914919 then
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ElopingDev/EloHub/refs/heads/main/EloHubFisch.lua"))()
 
+elseif universeId == 6504986360 then
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ElopingDev/EloHub/refs/heads/main/EloHubBGSI.lua"))()
 else
     print("Didn't find supported game")
 end
